9e870d03cd5b04b840b3e31d9d723c9a5030f8a8
Carlos V.M Lino <wenikius@gmail.com>
2026-04-06 16:50:28 -0300

    V0.9: Visor funcionando. Falta implementar as validações de input e consequentemente as funções matemáticas.

--- appsForms/Calculator.cs
+++ appsForms/Calculator.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using System.Windows.Forms.VisualStyles;
 
 namespace Sistema_De_Aplicativos_Simples__.NET.appsForms
@@ -8,13 +9,14 @@ namespace Sistema_De_Aplicativos_Simples__.NET.appsForms
         private Panel calculatorVisorPanel;
         private Label calculatorVisor;
         private Label visorTop;
-
-        private List<string> keyList;
+        private List<string> keyListTxt;
+        private IEnumerable<Button> btnList;
 
         public Calculator()
         {
             InitializeForm();
             InitializeFormComponents();
+            InitializeEvents();
             Calculation.CalculationTests();
         }
 
@@ -28,16 +30,15 @@ namespace Sistema_De_Aplicativos_Simples__.NET.appsForms
             MaximizeBox = false;
         }
 
-
         private void InitializeFormComponents()
         {
             InitializeAppLayout();
             InitializeVisorPanelUI();
 
-            keyList = new List<string> {
+            keyListTxt = new List<string> {
                 "%", "CE", "C", "DEL",
                 "1/x", "x²", "sqrt", "/",
-                "7", "8", "9", "X",
+                "7", "8", "9", "x",
                 "4", "5", "6", "-",
                 "1", "2", "3", "+",
                 "+/-", "0", ",", "="};
@@ -51,7 +52,14 @@ namespace Sistema_De_Aplicativos_Simples__.NET.appsForms
             calculatorVisorPanel.Controls.Add(visorTop);
             Controls.Add(calculatorVisorPanel);
             Controls.Add(keyboardLayout);
-            AppendCalculatorKeysToAppLayout(keyboardLayout, keyList);
+            AppendCalculatorKeysToAppLayout(keyboardLayout, keyListTxt);
+
+            btnList = GetCalculatorKeys(keyboardLayout);
+        }
+
+        private void InitializeEvents()
+        {
+            ApplyEventsToKeys(btnList);
         }
 
         private Label InitializeVisor()
@@ -65,11 +73,10 @@ namespace Sistema_De_Aplicativos_Simples__.NET.appsForms
                 ForeColor = Color.White,
                 Width = visorWidth,
                 Height = 50,
-                Text = "100 + 50",
+                Text = "0",
                 Font = new Font("Segoe UI", 16f, FontStyle.Bold),
                 TextAlign = System.Drawing.ContentAlignment.MiddleRight,
                 Left = calculatorVisorPanel.Width / 2 - visorCentralOffset,
-                //Top = calculatorVisorPanel.Height / 2
             };
 
             return visor;
@@ -113,7 +120,7 @@ namespace Sistema_De_Aplicativos_Simples__.NET.appsForms
             };
         }
 
-        private static void AppendCalculatorKeysToAppLayout(TableLayoutPanel layout, List<string> keyList)
+        private static void AppendCalculatorKeysToAppLayout(TableLayoutPanel layout, List<string> keyListTxt)
         {
             int counter = 0;
 
@@ -121,28 +128,109 @@ namespace Sistema_De_Aplicativos_Simples__.NET.appsForms
             {
                 for (int col = 0; col < layout.ColumnCount; col++)
                 {
-                    AppendCalculatorKey(layout, counter, col, row, keyList);
+                    AppendCalculatorKey(layout, counter, col, row, keyListTxt);
                     counter++;
                 }
             }
         }
 
-        private static void AppendCalculatorKey(TableLayoutPanel layout, int counter, int col, int row, List<string> keyList)
+        private static void AppendCalculatorKey(TableLayoutPanel layout, int counter, int col, int row, List<string> keyListTxt)
         {
-            var key = CreateCalculatorKey(counter, keyList);
+            var key = CreateCalculatorKey(counter, keyListTxt);
             layout.Controls.Add(key, col, row);
         }
 
-        private static Button CreateCalculatorKey(int counter, List<string> keyList)
+        private static Button CreateCalculatorKey(int counter, List<string> keyListTxt)
         {
             var key = new Button
             {
                 Dock = DockStyle.Fill,
-                Text = keyList[counter],
+                Text = keyListTxt[counter],
                 Font = new Font("Segoe UI", 12f, FontStyle.Bold),
                 ForeColor = Color.White
             };
             return key;
+        }
+
+        // Uso de recursão para capturar todos os elementos Button de um dado Control:
+        private IEnumerable<Button> GetCalculatorKeys(Control parent)
+        {
+            foreach (Control c in parent.Controls)
+            {
+                if (c is Button btn)
+                {
+                    yield return btn;
+                }
+
+                foreach (var child in GetCalculatorKeys(c))
+                {
+                    yield return child;
+                }
+            }
+        }
+
+        private void ClickKeyEvent(string text)
+        {
+            //TODO: criar validação de input (deve ser método separado):
+            List<string> numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+            List<string> operations = ["+", "-", "x", "/"];
+            List<string> special = ["%", "sqrt", "x²", "1/x", "CE", "C", "DEL", ","];
+
+            if (calculatorVisor.Text.Length == 20) return; // Pra pôr limite no número de caracteres no visor.
+
+            if (numbers.Contains(text))
+            {
+                Console.WriteLine($"{text} is a number!");
+
+                if (calculatorVisor.Text == "0") calculatorVisor.Text = text;
+                else calculatorVisor.Text += text;
+            }
+            else if (operations.Contains(text))
+            {
+                calculatorVisor.Text += $" {text} ";
+                switch (text)
+                {
+                    case "+":
+                        //TODO...
+                        break;
+                    case "-":
+                        //TODO...
+                        break;
+                    case "x":
+                        //TODO...
+                        break;
+                    case "/":
+                        //TODO...
+                        break;
+                }
+            }
+            else
+            {
+                switch (text)
+                {
+                    case "DEL":
+                        if (calculatorVisor.Text.Length != 1)
+                            calculatorVisor.Text = calculatorVisor.Text.Remove(calculatorVisor.Text.Length - 1);
+                        else calculatorVisor.Text = "0";
+                        break;
+                    case "CE":
+                        calculatorVisor.Text = "0";
+                        break;
+                    case "C":
+                        calculatorVisor.Text = "0";
+                        break;
+                }
+            }
+
+
+        }
+
+        private void ApplyEventsToKeys(IEnumerable<Button> btnList)
+        {
+            foreach (Button btn in btnList)
+            {
+                btn.Click += (_, _) => ClickKeyEvent(btn.Text);
+            }
         }
     }
 }
