905f2a7d0a864c1e22bf2689dacb83808ca20e8b
Carlos V.M Lino <wenikius@gmail.com>
2026-04-28 11:15:56 -0300

    V3.4: iniciado projeto UnitConverter.

--- appsForms/UnitConverter.cs
+++ appsForms/UnitConverter.cs
@@ -1,10 +1,28 @@
 //using Sistema_De_Aplicativos_Simples__.NET.appsForms;
 
+using Sistema_De_Aplicativos_Simples__.NET.appsForms;
+
 namespace Sistema_De_Aplicativos_Simples__.NET.appsForms
 {
     public partial class UnitConverter : Form
     {
+        public static UnitConverter Instance { get; private set; }
+        public UnitConverter()
+        {
+            Instance = this;
+            InitializeForm();
+            Components2.InitializeAppComponents();
+        }
 
+        private void InitializeForm()
+        {
+            Text = "Unit Converteer";
+            Width = 600;
+            Height = 400;
+            BackColor = Color.FromArgb(29, 49, 49);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+        }
     }
 }
 
@@ -12,5 +30,53 @@ namespace Sistema_De_Aplicativos_Simples__.NET.appsForms
 // seria melhor.
 public class Components2
 {
-    
+    public static Panel appPanel;
+    public static ComboBox unitCategory;
+    public static ComboBox fromUnit;
+    public static ComboBox toUnit;
+    public static TextBox fromNumber;
+    public static TextBox toNumber;
+
+    public static void InitializeAppComponents()
+    {
+        AppPanelConstructor();
+        unitCategory = ComboBoxConstructor(0, 1, new string[] { "m", "cm" }, 2);
+        fromUnit = ComboBoxConstructor(2, 0, null, 2);
+        toUnit = ComboBoxConstructor(2, 2, null, 2);
+
+        appPanel.Controls.Add(unitCategory);
+        appPanel.Controls.Add(fromUnit);
+        appPanel.Controls.Add(toUnit);
+        UnitConverter.Instance.Controls.Add(appPanel);
+    }
+
+    private static void AppPanelConstructor()
+    {
+        var formWidth = UnitConverter.Instance.Width;
+        appPanel = new Panel
+        {
+            Width = formWidth / 100 * 80, // 80% do tamanho do form.
+            Height = 240,
+            BorderStyle = BorderStyle.FixedSingle,
+            BackColor = Color.FromArgb(62, 85, 85)
+        };
+        appPanel.Left = formWidth / 100 * 10; // 10% do tamanho do form.
+        appPanel.Top = formWidth / 100 * 10;
+    }
+
+    private static ComboBox ComboBoxConstructor(int topOffset = 0, int leftOffset = 0, Array items = null, int maxItems = 1)
+    {
+        var comboBox = new ComboBox
+        {
+            Size = new Size(100, 20),
+        };
+        comboBox.Left = appPanel.Width / 2 - (100 * leftOffset) + comboBox.Width / 2;
+        comboBox.Top = appPanel.Height / 2 + (20 * topOffset) - comboBox.Height * 2;
+        comboBox.Items.AddRange(items != null ? items.Cast<Object>().ToArray() : new string[] { "" });
+        comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+        comboBox.SelectedIndex = 0;
+        comboBox.MaxDropDownItems = maxItems;
+
+        return comboBox;
+    }
 }
